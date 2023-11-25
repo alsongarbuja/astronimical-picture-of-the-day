69653edbf7e6977de7202821d60f6d72f8153591
alsongarbuja <garbuja12alson@gmail.com>
2023-11-25 17:55:44 +0545

    fix: fix the api fetch for each day revalidate and not use the build cache call

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,6 +1,10 @@
 
 export const getPictureToday = async (): Promise<Picture> => {
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`);
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`, {
+    next: {
+      revalidate: 60 * 60 * 10, // 10 hours
+    }
+  });
   const data = await response.json();
   return data;
 }
